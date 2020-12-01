b0955a2bbb75c37bf47569894f970928fab6851e
Logicee <67381721+Logicee@users.noreply.github.com>
2020-12-01 13:29:00 -0400

    Add files via upload

---
+++ mainjava.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+window.onload = function () {
+
+    // add event handlers for buttons
+    document.querySelector("#Guest").addEventListener("click", guestPage);
+    document.querySelector("#Score").addEventListener("click", ScorePage);
+    document.querySelector("#ad").addEventListener("click", AdminPage);}
+    
+//Guest , Score, ad
+    // add event handler for selections on the table
+function guestPage() {
+    
+}
