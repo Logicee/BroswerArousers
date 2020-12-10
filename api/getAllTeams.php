f441e864512d016a5b9d23c13f7477841b3186f7
DerekHollis <70291569+DerekHollis@users.noreply.github.com>
2020-12-10 14:43:56 -0400

    Update getAllTeams.php

--- api/getAllTeams.php
+++ api/getAllTeams.php
@@ -1,10 +1,10 @@
 <?php
 $projectRoot = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/BABowlingApp';
-require_once ($projectRoot . '/accessors/TeamAccessor.php');
-
+require_once ($projectRoot . '/db/TeamAccessor.php');
+require_once ($projectRoot . '/entity/Team.php');
 try {
-    $mia = new TeamAccessor();
-    $results = $mia->getAllTeams(); // these are objects
+    $ta = new TeamAccessor();
+    $results = $ta->getAllTeams();
     $results = json_encode($results, JSON_NUMERIC_CHECK);
     echo $results;
 } catch (Exception $e) {
