532f60053aa57d0e49dc7aa39d2c29d52788f4a8
Logicee <67381721+Logicee@users.noreply.github.com>
2020-12-10 14:58:10 -0400

    Update insertTeam.php

--- api/insertTeam.php
+++ api/insertTeam.php
@@ -3,7 +3,7 @@
 $projectRoot = $_SERVER['DOCUMENT_ROOT'] . '/BABowlingApp';
 require_once ($projectRoot . '/accessors/TeamAccessor.php');
 require_once ($projectRoot . '/entity/Team.php');
-
+require_once ($projectRoot . '/ChromePhp.php');
 // reading the HTTP request body
 //$body = file_get_contents('php://input');
 //$contents1 = json_decode($body, true);
@@ -11,12 +11,15 @@ require_once ($projectRoot . '/entity/Team.php');
 // create a team object
 $teamID = $_GET['teamID'];
 $teamName = $_GET['teamName'];
-$CarObj = new Team(51, "sdf", Null);
+$teamObj = new Team($teamID, $teamName, 0);
 
+ChromePhp::log("//////////////////Hello");
 // add the object to DB
 try {
     $mia = new TeamAccessor();
-    $success = $mia->insertItem($CarObj);
+    ChromePhp::log("//////////////////Before");
+    $success = $mia->insertItem($teamObj);
+    ChromePhp::log("//////////////////After");
     echo $success;
 } catch (Exception $e) {
     echo "ERROR " . $e->getMessage();
@@ -25,4 +28,4 @@ try {
 //redirects to adminIndex
 $projectRoot = filter_input('DOCUMENT_ROOT') . '/BABowlingApp';
 header("Location:". $projectRoot."/adminIndex.php");
-?>
+
