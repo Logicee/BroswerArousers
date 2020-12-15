bd9e6a18842ffab525cedb9be7d596d3e4353948
DerekHollis <70291569+DerekHollis@users.noreply.github.com>
2020-12-15 17:19:25 -0400

    Update tournamentStandings.php

--- tournamentStandings.php
+++ tournamentStandings.php
@@ -5,12 +5,36 @@
         <meta charset="UTF-8">
         <title>Tournament Standings</title>
 
-        <link rel="stylesheet" href="tournamentStandings.css"/>
+      
         <script src="tournamentStandings.js"></script>  
-
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="bowling1.css"/>
+        <link rel="stylesheet" href="tournamentStandings.css"/>
     </head>
     <body>
-
+      <div class=" justify-content-center row mt-2">
+            <div class="col-10">
+                <h1>Bowling Tournament Standings</h1>
+            </div>
+            <div class="col-1-auto">
+                <form action="index.php">
+                    <button class="mt-4 mr-3">Log Out</button>  
+                </form>
+                
+                
+            </div>
+            <div class="col-1-auto">
+                <img id="logo" src="bowling.png" class="img-thumbnail rounded float-right" alt="Bowling App logo"/>
+                
+            </div>
+        </div>
+               <div class=" justify-content-center row mt-2 mb-4 border-bottom">
+            <div class="col-12">
+                <form action="disha.php"><button class="mb-2">Back</button></form>
+            </div>
+         
+        </div>
         <div class="brackets" id="brackets">
             <div class="wrappingdiv">
 
