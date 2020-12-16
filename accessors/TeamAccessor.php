593c283b3a07cee8b36d0cce5b9945b884f6e6b0
ultimatemandan <64611632+ultimatemandan@users.noreply.github.com>
2020-12-16 12:07:38 -0400

    Update TeamAccessor.php
    admin teams' accessor

--- accessors/TeamAccessor.php
+++ accessors/TeamAccessor.php
@@ -1,22 +1,36 @@
 <?php
+
+//require_once 'dbconnection.php';
+//require_once 'entity/Team.php';
+//require_once 'PlayerAccessor.php';
+//require_once 'entity/Player.php';
+
+$projectRoot = $_SERVER['DOCUMENT_ROOT'] . '/BABowlingApp';
 require_once 'dbconnection.php';
-require_once 'entity/Team.php';
-require_once 'PlayerAccessor.php';
-require_once 'entity/Player.php';
+require_once ($projectRoot . '/entity/Team.php');
+require_once ($projectRoot . '/entity/Player.php');
+require_once ($projectRoot . '/ChromePhp.php');
 
 class TeamAccessor {
 
-      private $getGamesByStatusStatementString = "select * from game where gameStateID IN(\"AVAILABLE\",\"INPROGRESS\")";
-   
+    private $getGamesByStatusStatementString = "select * from game where gameStateID IN(\"AVAILABLE\",\"INPROGRESS\")";
+    private $getByIDStatementString = "select * from team where teamID = :number";
+    private $insertStatementString = "INSERT INTO `team` (`teamID`, `teamName`) VALUES (:teamID, :teamName)";
+    private $deleteStatementString = "delete from team where teamID = :number";
+    private $updateStatementString = "update team set teamName = :name where teamID = :id";
+    private $deletePlayerString = "delete from player where playerID = :playerNum";
+    private $deletePlayer = NULL;
+    private $updateStatement = NULL;
+    private $deleteStatement = NULL;
+    private $getByIDStatement = NULL;
+    private $insertStatement = NULL;
     private $conn = NULL;
     private $getGamesByStatusStatement = NULL;
- 
-
 
     public function __construct() {
-        $connect = new dbConnect();
+        $cm = new dbConnect();
 
-        $this->conn = $connect->connectToDB();
+        $this->conn = $cm->connectToDB();
         if (is_null($this->conn)) {
             throw new Exception("no connection");
         }
@@ -24,11 +38,28 @@ class TeamAccessor {
         if (is_null($this->getGamesByStatusStatement)) {
             throw new Exception("bad statement: '" . $this->getGamesByStatusStatement . "'");
         }
-        //add additional prepares here
-
+        $this->getByIDStatement = $this->conn->prepare($this->getByIDStatementString);
+        if (is_null($this->getByIDStatement)) {
+            throw new Exception("bad statement: '" . $this->getAllStatementString . "'");
+        }
+        $this->insertStatement = $this->conn->prepare($this->insertStatementString);
+        if (is_null($this->insertStatement)) {
+            throw new Exception("bad statement: '" . $this->getAllStatementString . "'");
+        }
+        $this->deleteStatement = $this->conn->prepare($this->deleteStatementString);
+        if (is_null($this->deleteStatement)) {
+            throw new Exception("bad statement: '" . $this->deleteStatementString . "'");
+        }
+        $this->updateStatement = $this->conn->prepare($this->updateStatementString);
+        if (is_null($this->updateStatement)) {
+            throw new Exception("bad statement: '" . $this->updateStatementString . "'");
+        }
+        $this->deletePlayer = $this->conn->prepare($this->deletePlayerString);
+        if (is_null($this->deletePlayer)) {
+            throw new Exception("bad statement: '" . $this->deletePlayerString . "'");
+        }
     }
 
-  
     private function getTeamsByQuery($selectString) {
         $result = [];
 
@@ -36,56 +67,136 @@ class TeamAccessor {
             $stmt = $this->conn->prepare($selectString);
             $stmt->execute();
             $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $playerAccess = new PlayerAccessor();
 
-            foreach ($dbresults as $record) {
-                $teamID = $record['teamID'];
-                $teamName = $record['teamName'];
-                $earnings = $record['earnings'];
-                $team = new Team($teamID, $teamName, $earnings, $playerAccess->getTeamPlayers($teamID));
-                
-                array_push($result, $team);
+            foreach ($dbresults as $r) {
+                $teamID = $r['teamID'];
+                $teamName = $r['teamName'];
+                $playerCount = $r['Total_players'];
+                $obj = new Team($teamID, $teamName, $playerCount);
+                array_push($result, $obj);
             }
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             $result = [];
-        }
-        finally {
+        } finally {
             if (!is_null($stmt)) {
                 $stmt->closeCursor();
             }
         }
 
         return $result;
-    }//end getGamesByQuery()
-    
+    }
+
+    public function getAllTeams() {
+        // return $this->getTeamsByQuery("select t.teamName, p.teamID, count(*) as Total_players from team t, player p where t.teamID = p.teamID group by t.teamID");
+        return $this->getTeamsByQuery("select t.teamName, t.teamID, count(p.playerID) as Total_players from team t  LEFT JOIN player p ON t.teamID = p.teamID group by t.teamID;");
+    }
+
     public function getTeamByID($id) {
-  
+
         //return $this->getTeamsByQuery("select * from team where teamID = $id");
-        $team = []; 
+        $team = [];
         try {
             $stmt = $this->conn->prepare("select * from team where teamID = $id");
             $stmt->execute();
             $record = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
             $playerAccess = new PlayerAccessor();
-            
-                $teamID = $record['teamID'];
-                $teamName = $record['teamName'];
-                $earnings = $record['earnings'];
-                $team = new Team($teamID, $teamName, $earnings, $playerAccess->getTeamPlayers($teamID));
-            
-            
-        }
-        catch (Exception $e) {
+
+            $teamID = $record['teamID'];
+            $teamName = $record['teamName'];
+            $earnings = $record['earnings'];
+            $team = new Team($teamID, $teamName, $earnings, $playerAccess->getTeamPlayers($teamID));
+        } catch (Exception $e) {
             $team = [];
-        }
-        finally {
+        } finally {
             if (!is_null($stmt)) {
                 $stmt->closeCursor();
             }
         }
-    
-    return $team;
+
+        return $team;
     }
-}//end class
+    
+    public function insertItem($item) {
+        $success = false;
+
+        $teamID = $item->getTeamID();
+        $teamName = $item->getTeamName();
+        $earnings = $item->getPlayerCount();
+        ChromePhp::log("The values are: " . $teamID . $teamName . $earnings);
+        try {
+            $this->insertStatement->bindParam(":teamID", $teamID);
+            $this->insertStatement->bindParam(":teamName", $teamName);
+            // $this->insertStatement->bindParam(":earnings", $earnings);
+            $success = $this->insertStatement->execute(); // this doesn't mean what you think it means
+        } catch (PDOException $e) {
+            $success = false;
+        } finally {
+            if (!is_null($this->insertStatement)) {
+                $this->insertStatement->closeCursor();
+            }
+            return $success;
+        }
+    }
+
+    public function deleteItem($item) {
+        $success = false;
+
+        $TeamID = $item->getTeamID(); // only the ID is needed
+
+        try {
+            $this->deleteStatement->bindParam(":number", $TeamID);
+            $success = $this->deleteStatement->execute(); // this doesn't mean what you think it means
+            $rc = $this->deleteStatement->rowCount();
+        } catch (PDOException $e) {
+            $success = false;
+        } finally {
+            if (!is_null($this->deleteStatement)) {
+                $this->deleteStatement->closeCursor();
+            }
+            return $success;
+        }
+    }
+
+    public function updateItem($item) {
+        $success = false;
+
+        $teamID = $item->getTeamID();
+        $teamName = $item->getTeamName();
+        try {
+            $this->updateStatement->bindParam(":name", $teamName);
+            $this->updateStatement->bindParam(":id", $teamID);
+            $success = $this->updateStatement->execute(); // this doesn't mean what you think it means
+        } catch (PDOException $e) {
+            $success = false;
+        } finally {
+            if (!is_null($this->updateStatement)) {
+                $this->updateStatement->closeCursor();
+            }
+            return $success;
+        }
+    }
+
+    public function deletePlayer($item) {
+        $success = false;
+
+        $PlayerID = $item->getPlayerID(); // only the ID is needed
+
+        try {
+            $this->deletePlayer->bindParam(":playerNum", $PlayerID);
+            $success = $this->deletePlayer->execute(); // this doesn't mean what you think it means
+            $rc = $this->deletePlayer->rowCount();
+        } catch (PDOException $e) {
+            $success = false;
+        } finally {
+            if (!is_null($this->deletePlayer)) {
+                $this->deletePlayer->closeCursor();
+            }
+            return $success;
+        }
+    }
+    
+
+}
+
+//end class
 
